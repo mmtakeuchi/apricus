4484ed991e18c0574963e4c28e5ee401ec53598f
mmtakeuchi <mikemtaki@gmail.com>
2020-09-15 09:18:33 -0700

    create review class js

---
+++ frontend/models/review.js
@@ -0,0 +1,8 @@
+class Review {
+    constructor(id, username, content, recommend) {
+        this.id = id;
+        this.username = username;
+        this.content = content;
+        this.recommend = recommend
+    }
+}
