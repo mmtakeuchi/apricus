ffc7a1792c3a95c0b20086419355641343a64b27
mmtakeuchi <mikemtaki@gmail.com>
2020-09-15 11:38:23 -0700

    fetch reviews from form

--- frontend/models/review.js
+++ frontend/models/review.js
@@ -16,32 +16,55 @@ class Review {
         let reviewForm = document.createElement("form")
         reviewForm.setAttribute("id", "reviewForm")
 
-        let usernameForm = document.createElement("div")
-        usernameForm.innerHTML = "<label for='username'>Nickname: </label>";
-        usernameForm.innerHTML += "<input type='text' id='username' name='username'>";
+        let usernameInput = document.createElement("div")
+        usernameInput.innerHTML = "<label for='username'>Nickname: </label>";
+        usernameInput.innerHTML += "<input type='text' id='username' name='username'>";
 
-        let contentForm = document.createElement("div")
-        contentForm.innerHTML = "<label for='content'>Review: </label>";
-        contentForm.innerHTML += "<textarea id='content' name='content'></textarea>";
+        let contentInput = document.createElement("div")
+        contentInput.innerHTML = "<label for='content'>Review: </label>";
+        contentInput.innerHTML += "<textarea id='content' name='content'></textarea>";
 
-        let recommendForm = document.createElement("div")
-        recommendForm.innerHTML = "<label for='recommend'>Recommend? </label>"
-        recommendForm.innerHTML += "<input type='radio' name='recommend' value='yes'> Yes";
-        recommendForm.innerHTML += "<input type='radio' name='recommend' value='no'> No";
+        let recommendInput = document.createElement("div")
+        recommendInput.innerHTML = "<label for='recommend'>Recommend? </label>"
+        recommendInput.innerHTML += "<input type='radio' name='recommend' value='yes'> Yes";
+        recommendInput.innerHTML += "<input type='radio' name='recommend' value='no'> No";
 
         let submitReview = document.createElement("input")
         submitReview.setAttribute("type", "submit")
         submitReview.value = "Add Review";
         submitReview.addEventListener("click", Review.addReview) 
 
-        reviewForm.appendChild(usernameForm);
-        reviewForm.appendChild(contentForm);
-        reviewForm.appendChild(recommendForm);
+        reviewForm.appendChild(usernameInput);
+        reviewForm.appendChild(contentInput);
+        reviewForm.appendChild(recommendInput);
         reviewForm.appendChild(submitReview);
         div.appendChild(reviewForm);
     }
 
-    static addReview() {
-        
+    static addReview(e) {
+        e.preventDefault();
+
+        const strongParams = {
+            review: {
+                username: usernameInput.value,
+                username: contentInput.value,
+                username: recommendInput.value,
+            }
+        }
+
+        fetch(baseUrl + "/reviews", {
+            method: "POST",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(strongParams)
+        })
+        .then(resp => resp.json())
+        .then(data => {
+            let review = Review.create(data.id, data.username, data.content, data.recommend);
+            review.renderReview();
+        })
+        .catch(errors => console.log(errors))
     }
 }
